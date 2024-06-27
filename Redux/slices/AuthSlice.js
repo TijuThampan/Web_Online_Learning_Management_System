b3ba6b3be06629cce409c4fd7cb96e84d902f542
TijuThampan <tijuthampan96@gmail.com>
2024-06-27 15:46:17 +0100

    code for user authentication

--- Redux/slices/AuthSlice.js
+++ Redux/slices/AuthSlice.js
@@ -30,5 +30,132 @@ export const signup = createAsyncThunk("/auth/signup", async (data) => {
         throw error;
     }
 });
-
+export const login = createAsyncThunk("/auth/login", async (data) => {
+    try {
+        toast.loading("Wait! login in your account", {
+            position: 'top-center'
+        });
+        const response = await axiosInstance.post('/user/login', data);
+        if (response.status === 200) {
+            toast.dismiss();
+            toast.success(response.data.message);
+            return response.data;
+        } else {
+            toast.dismiss();
+            toast.error(response.data.message);
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+        throw error;
+    }
+});
+export const logout = createAsyncThunk("/auth/logout", async () => {
+    try {
+        toast.loading("Wait! logout in progress", {
+            position: 'top-center'
+        });
+        const response = await axiosInstance.get('/user/logout');
+        if (response.status === 200) {
+            toast.dismiss();
+            toast.success(response.data.message);
+            return response.data;
+        } else {
+            toast.dismiss();
+            toast.error(response.data.message);
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+        throw error;
+    }
+});
+export const forgotPassword = createAsyncThunk("/user/forgotPassword", async (data) => {
+    try {
+        toast.loading("Wait! sending request...", {
+            position: 'top-center'
+        });
+        const response = await axiosInstance.post('/user/forgot-password', data)
+        if (response.status === 200) {
+            toast.dismiss();
+            toast.success(response.data.message);
+            return response.data;
+        } else {
+            toast.dismiss();
+            toast.error(response.data.message);
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+        throw error;
+    }
+});
+export const resetPassword = createAsyncThunk("/user/resetPassword", async (data) => {
+    try {
+        toast.loading("Wait! resetting password...", {
+            position: 'top-center'
+        });
+        const response = await axiosInstance.post(`/user/reset/${data.resetToken}`, {
+            password: data.password
+        });
+        if (response.status === 200) {
+            toast.dismiss();
+            toast.success(response.data.message);
+            return response.data;
+        } else {
+            toast.dismiss();
+            toast.error(response.data.message);
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+        throw error;
+    }
+});
+export const changePassword = createAsyncThunk("/user/changePassword", async (data) => {
+    try {
+        toast.loading("Wait! changing password..", {
+            position: 'top-center'
+        });
+        const response = await axiosInstance.put('/user/change-password', data);
+        if (response.status === 200) {
+            toast.dismiss();
+            toast.success(response.data.message);
+            return response.data;
+        } else {
+            toast.dismiss();
+            toast.error(response.data.message);
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+        throw error;
+    }
+});
+export const editProfile = createAsyncThunk("/user/editProfile", async (data) => {
+    try {
+        toast.loading("Wait! update profile", {
+            position: 'top-center'
+        });
+        const response = await axiosInstance.put('/user/update', data);
+        if (response.status === 200) {
+            toast.dismiss();
+            toast.success(response.data.message);
+            return response.data;
+        } else {
+            toast.dismiss();
+            toast.error(response.data.message);
+            throw new Error(response.data.message);
+        }
+    } catch (error) {
+        toast.dismiss();
+        toast.error(error?.response?.data?.message);
+        throw error;
+    }
+});
 export default authSlice.reducer
