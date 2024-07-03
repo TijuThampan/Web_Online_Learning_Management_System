e3b0e3751b87070917405ad8fa828e1fa928a78d
TijuThampan <tijuthampan96@gmail.com>
2024-07-03 14:40:44 +0100

    New changes added.

--- Helpers/AxiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-const axiosInstance = axios.create();
-axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;
-
-export default axiosInstance
