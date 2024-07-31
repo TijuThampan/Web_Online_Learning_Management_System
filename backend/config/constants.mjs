d02ba2668b0efb06cb927e0d4c8da0c25b5e25dd
TijuThampan <tijuthampan96@gmail.com>
2024-07-31 02:02:12 +0100

    DB connection resolved

--- backend/config/constants.mjs
+++ backend/config/constants.mjs
@@ -1,3 +1,3 @@
 export const JWT_SECRET = "jsonwebtoken";
 export const MONGO_DB_URL =
-  "mongodb+srv://jeevanandh:%3DUpC%40%3B!33E2l@ims.tq2gleb.mongodb.net/";
+  "mongodb+srv://tijuthampan96:lzbXT7yUFdKDsrP7@lms.f8pnrqu.mongodb.net/";
